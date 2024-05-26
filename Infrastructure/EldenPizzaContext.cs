5e6ae878700707b110547f1267b008a93049ea28
Pc <Pc@DESKTOP-NTLN2TN>
2024-05-26 02:51:19 -0600

    Add project files.

---
+++ Infrastructure/EldenPizzaContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using POS.Data.Entities;
+using POS.Data.Maps;
+
+namespace EldenPizza.POS.WebApi.Infrastructure
+{
+    public class EldenPizzaContext : DbContext
+    {
+
+        public DbSet<TipoProducto> TiposProducto => Set<TipoProducto>();
+        public DbSet<Producto> Productos => Set<Producto>();
+
+        public EldenPizzaContext(DbContextOptions<EldenPizzaContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new TipoProductoMap());
+            modelBuilder.ApplyConfiguration(new ProductoMap());
+        }
+    }
+ }
